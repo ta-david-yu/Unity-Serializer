d8f2bba9a16eb4b13bd006a0c6fb36352e1648e2
Ta David Yu <d366095@gmail.com>
2020-06-27 00:34:52 +0200

    Added SerializeRefUI attribute to show poly fieldtype

---
+++ Runtime/InterfaceContainer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DYSerializer
+{
+    public abstract class InterfaceContainer
+    {
+        [SerializeField]
+        protected object m_Field;
+    }
+
+    public abstract class InterfaceContainer<TInterface> : InterfaceContainer, ISerializationCallbackReceiver
+    {
+        private System.Type m_Type;
+        public System.Type Type
+        {
+            get { return m_Type; }
+            set
+            {
+                if (value != null && !value.IsClass)
+                    throw new ArgumentException(string.Format("'{0}' is not a class type.", value.FullName), "value");
+
+                m_Type = value;
+                m_TypeName = (value == null) ? "" : value.AssemblyQualifiedName;
+            }
+        }
+
+        [SerializeField]
+        private string m_TypeName;
+
+        private TInterface m_Value;
+
+        public TInterface Value
+        {
+            get
+            {
+                return m_Value;
+            }
+
+            set
+            {
+                // TODO
+            }
+        }
+
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+
+        }
+    }
+}
